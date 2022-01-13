1543e9478946b6fa51d87bb775c40c77606a6089
Florencia Silva <florencia.silva@dowjones.com>
2022-01-13 01:10:33 -0600

    fix: empty files

--- src/Search.js
+++ src/Search.js
@@ -1,41 +1,5 @@
-import { useState } from 'react'
-
-
-
-const Search = ({resultList = ['Luke Skywalker', 'R2-D2', 'Obi Wan Kenobi', 'Luke', 'Luke2', 'Luke3']}) => {
-    const [ searchText, setSearchText ] = useState('')
-    const [ list, setList ] = useState([])
-
-    const handleSearchClick = () => {
-        if(searchText.length > 0) {
-            const filterList = resultList.filter(list => list.toLowerCase().includes(searchText.toLowerCase()))
-            setList(filterList)
-        }
-    }
-
-    const mapList = () => {
-        if(list.length > 0) {
-            const determineItemAmount = () => {
-                if(list.length > 3 ) list.length = 3
-                return list
-            }
-
-            const mapListItems = determineItemAmount().map(listItem => 
-                <li key={listItem}>{listItem}</li>
-            )
+const Search = () => {
     
-            return <ul>{mapListItems}</ul>
-        }
-        return null
-    }
-    
-    return (
-        <div>
-            <input type="text" onChange={(event) => setSearchText(event.target.value)}/>
-            <button onClick={handleSearchClick}>Search</button>
-            {mapList()}
-        </div>
-    )
 }
 
 export default Search
--- src/Search.test.js
+++ src/Search.test.js
@@ -2,56 +2,7 @@ import { render, fireEvent } from '@testing-library/react';
 import Search from './Search';
 
 describe('Search Component', () => {
-    test('renders an input and a button', () => {
-        const { getByRole } = render(<Search />)
-
-        getByRole('textbox')
-        getByRole('button', { value: 'Search' })
-    })
-    test('Input should be able to allow writing', () => {
-        const { getByRole } = render(<Search />)
-        const input = getByRole('textbox')
-
-        fireEvent.change(input, {target: {value: 'Luke Skywalker'}})
-
-        expect(input.value).toBe('Luke Skywalker')
-    })
-    test('Search button should show input writing on screen after pressing Search Button', () => {
-        const { getByRole, getByText } = render(<Search resultList={['Luke Skywalker', 'R2-D2', 'Obi Wan Kenobi']}/>)
-        const input = getByRole('textbox')
-        const button = getByRole('button')
-
-        fireEvent.change(input, {target: {value: 'R2-D2'}})
-        fireEvent.click(button)
-
-        getByText('R2-D2')
-    })
-    test('Search button shouldnt allow for empty requests', () => {
-        const { getByRole, queryByRole } = render(<Search resultList={['Luke Skywalker', 'R2-D2', 'Obi Wan Kenobi']}/>)
-        const input = getByRole('textbox')
-        const button = getByRole('button')
-        const list = queryByRole('list')
-
-        fireEvent.change(input, {target: {value: ''}})
-        fireEvent.click(button)
-
-
-        expect(list).not.toBeInTheDocument()
-        
-    })
-
-    test('Search result list should have no more than three results on screen', () => {
-        const { getByRole } = render(<Search resultList={['Luke Skywalker', 'R2-D2', 'Obi Wan Kenobi', 'Luke', 'Luke2', 'Luke3']}/>)
-        const input = getByRole('textbox')
-        const button = getByRole('button')
-
-        fireEvent.change(input, {target: {value: 'Luke'}})
-        fireEvent.click(button)
-
-        const list = getByRole('list')
-
-        expect(list.childNodes.length).toBe(3)
-    })
+    
 })
 
 
